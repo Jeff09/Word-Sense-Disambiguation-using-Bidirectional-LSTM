7d2554293262591e0a0963ac66f379c2c575edf0
Jeff09 <likun0203@hotmail.com>
2017-12-13 11:03:09 -0600

    Add files via upload

--- model4.py
+++ model4.py
@@ -37,28 +37,28 @@ test_path = '/data/senseval2/eng-lex-samp.evaluation.xml'
 # load data
 #train_data = load_senteval2_data(train_path, True)
 #test_data = load_senteval2_data(test_path, False)
-train_data = load_train_data(23)
+train_data_ = load_train_data(23)
 test_data = load_test_data(2)
-print('Dataset size (train/test): %d / %d' % (len(train_data), len(test_data)))
+print('Dataset size (train/test): %d / %d' % (len(train_data_), len(test_data)))
 
 EMBEDDING_DIM = 100
 print('Embedding vector: %d' % EMBEDDING_DIM)
 
 # build vocab utils
-word_to_id = build_vocab(train_data)
-target_word_to_id, target_sense_to_id, n_words, n_senses_from_target_id = build_sense_ids(train_data)
+word_to_id = build_vocab(train_data_)
+target_word_to_id, target_sense_to_id, n_words, n_senses_from_target_id = build_sense_ids(train_data_)
 print('Vocabulary size: %d' % len(word_to_id))
 
 #build context vocab of the target sense
-train_target_sense_to_context = build_context(train_data, word_to_id)
+train_target_sense_to_context = build_context(train_data_, word_to_id)
 
 #build context embeddings of the target sense
 embedding_matrix = fill_with_gloves(word_to_id, 100)
 target_sense_to_context_embedding = build_embedding(train_target_sense_to_context, embedding_matrix, len(word_to_id), EMBEDDING_DIM)
 
 # make numeric
-train_ndata = convert_to_numeric(train_data, word_to_id, target_word_to_id, target_sense_to_id, n_senses_from_target_id, target_sense_to_context_embedding, is_training = True)
-test_ndata = convert_to_numeric(test_data, word_to_id, target_word_to_id, target_sense_to_id, n_senses_from_target_id, target_sense_to_context_embedding, is_training = False)
+train_ndata = convert_to_numeric(train_data_, word_to_id, target_word_to_id, target_sense_to_id, n_senses_from_target_id, target_sense_to_context_embedding, is_training = True)
+#test_ndata = convert_to_numeric(test_data, word_to_id, target_word_to_id, target_sense_to_id, n_senses_from_target_id, target_sense_to_context_embedding, is_training = False)
 
 n_step_f = 40
 n_step_b = 40
@@ -131,12 +131,12 @@ def own_model(train_forward_data, train_backward_data, train_sense_embedding,
 if __name__ == '__main__':
 
     grouped_by_target = group_by_target(train_ndata)
-    train_data, val_data = split_grouped(grouped_by_target, .2, 2)
+    train_data, val_data = split_grouped(grouped_by_target, 0)
 
     init_emb = fill_with_gloves(word_to_id, EMBEDDING_DIM)
     
     train_forward_data, train_backward_data, train_target_sense_ids, train_sense_embedding = get_data(train_data, n_step_f, n_step_b)
-    val_forward_data, val_backward_data, val_target_sense_ids, val_sense_embedding = get_data(train_data, n_step_f, n_step_b)
+    val_forward_data, val_backward_data, val_target_sense_ids, val_sense_embedding = get_data(val_data, n_step_f, n_step_b)
         
     own_model(train_forward_data, train_backward_data, train_sense_embedding, 
               val_forward_data, val_backward_data, val_sense_embedding, 
